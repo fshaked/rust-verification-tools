33c218f4eedd96e5230159c748e4c077742dcab3
Alastair Reid <adreid@google.com>
2021-01-20 18:23:10 +0000

    AOC day 1: add tests for second part

--- aoc/2015/day1/src/main.rs
+++ aoc/2015/day1/src/main.rs
@@ -1,9 +1,14 @@
+#[allow(unused_imports)]
 use std;
 
 fn main() {
     for dirs in std::env::args() {
         println!("Santa {} = {}", dirs, santa(&dirs));
-        println!("Santa goes underground {} at step {}", dirs, underground_santa(&dirs))
+        println!(
+            "Santa goes underground {} at step {:?}",
+            dirs,
+            underground_santa(&dirs)
+        )
     }
 }
 
@@ -22,28 +27,7 @@ fn santa(dirs: &str) -> isize {
     count
 }
 
-// part two of the puzzle
-fn underground_santa(dirs: &str) -> usize {
-    let mut count = 0;
-    for (i, c) in dirs.chars().enumerate() {
-        // todo: I don't like this repetition from previous solution much
-        // todo: should we consider a solution that answers both puzzles
-        // in a single pass?
-        if c == '(' {
-            count += 1;
-        } else if c == ')' {
-            count -= 1;
-        } else {
-        }
-        if count < 0 {
-            return i+1
-        }
-    }
-    return 0; // todo: is this the right way to indicate error?
-
-}
-
-mod test {
+mod test_part1 {
     #[cfg(not(verify))]
     use proptest::prelude::*;
     #[cfg(verify)]
@@ -125,6 +109,120 @@ mod test {
         #[test]
         fn filtered2(x: String) {
             prop_assert_eq!(santa(x.as_str()), santa_spec(x.as_str()))
+        }
+    }
+}
+
+// part two of the puzzle
+fn underground_santa(dirs: &str) -> Option<usize> {
+    let mut count = 0;
+    for (i, c) in dirs.chars().enumerate() {
+        // todo: I don't like this repetition from previous solution much
+        // todo: should we consider a solution that answers both puzzles
+        // in a single pass?
+        if c == '(' {
+            count += 1;
+        } else if c == ')' {
+            count -= 1;
+        } else {
+        }
+        if count < 0 {
+            return Some(i + 1);
+        }
+    }
+    None
+}
+
+mod test_part2 {
+    #[cfg(not(verify))]
+    use proptest::prelude::*;
+    #[cfg(verify)]
+    use propverify::prelude::*;
+
+    use super::{santa, underground_santa};
+
+    #[test]
+    fn empty() {
+        assert_eq!(underground_santa(""), None) // 0 indicates failure
+    }
+
+    proptest! {
+        /// Test that adding one character either
+        /// - returns previous result if already found basement; or
+        /// - reaches the basement if we are at floor zero and we go down; or
+        /// - still haven't reached the basement
+        #[test]
+        fn push(x: String, c: char) {
+            let r = if let Some(rx) = underground_santa(x.as_str()) {
+                Some(rx)
+            } else if santa(x.as_str()) == 0 && c == ')' {
+                // Incorrect first attempt used len (number of bytes) instead of
+                // number of characters
+                // Some(x.len() + 1)
+                Some(x.chars().count() + 1)
+            } else {
+                None
+            };
+            let mut x = x;
+            x.push(c);
+            prop_assert_eq!(underground_santa(x.as_str()), r)
+        }
+    }
+
+    proptest! {
+        /// Test that if we have already reached the basement in `x`,
+        /// then we stop digging no matter what is in `y`.
+        #[test]
+        fn append(x: String, y: String) {
+            if underground_santa(x.as_str()).is_some() {
+                prop_assert_eq!(
+                    underground_santa(x.as_str()),
+                    underground_santa((x + &y).as_str()))
+            }
+        }
+    }
+
+    proptest! {
+        /// Various lower and upper bounds on the result
+        #[test]
+        fn length(x: String) {
+            match underground_santa(x.as_str()) {
+                Some(rx) => {
+                    prop_assert!(1  <= rx);
+                    prop_assert!(rx <= x.len());
+                    prop_assert!(rx <= x.chars().count())
+                },
+                _ =>
+                    ()
+            }
+        }
+    }
+
+    proptest! {
+        /// We must be going down when we hit the basement
+        #[test]
+        fn going_down(x: String) {
+            match underground_santa(x.as_str()) {
+                Some(rx) => {
+                    prop_assert_eq!(x.chars().nth(rx-1).unwrap(), ')')
+                },
+                _ => {
+                }
+            }
+        }
+    }
+
+    proptest! {
+        /// The value must be -1 when we hit the basement
+        #[test]
+        fn basement1(x: String) {
+            match underground_santa(x.as_str()) {
+                Some(rx) => {
+                    prop_assert_eq!(Some(rx), underground_santa(x.chars().take(rx).collect::<String>().as_str()))
+                },
+                _ => {
+                }
+            }
         }
     }
 }
